8cfd6acaee4721a06921d91a005d2e702823f319
Igor Souza Carvalho <igmalte2@gmail.com>
2026-04-20 18:03:53 -0300

    Create page ActivityUser and action for button

--- app/src/main/java/com/montyblank/motivation/UserActivity.kt
+++ app/src/main/java/com/montyblank/motivation/UserActivity.kt
@@ -1,20 +1,45 @@
 package com.montyblank.motivation
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.montyblank.motivation.databinding.ActivityUserBinding
 
-class UserActivity : AppCompatActivity() {
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    private lateinit var binding: ActivityUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        binding= ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         setContentView(R.layout.activity_user)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setListeners()
+
     }
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save ){
+            handleSave()
+        }
+    }
+    private fun handleSave(){
+
+    }
+    private fun setListeners(){
+
+    }
+
 }
